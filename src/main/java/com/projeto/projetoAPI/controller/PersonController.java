b70574fd570474b5e57cd3c8363e634fbcd2683b
Loukis-13 <ze.zinho-2010@hotmail.com>
2021-06-16 16:31:25 -0300

    first

---
+++ src/main/java/com/projeto/projetoAPI/controller/PersonController.java
@@ -0,0 +1,14 @@
+package com.projeto.projetoAPI.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/person")
+public class PersonController {
+    @GetMapping
+    public String getBook () {
+        return "Projeto API Java";
+    }
+}
